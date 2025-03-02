5cfebed9bb969c9525546d4e30f9ccdd40a51a3e
nitin-rachabathuni <reachnitinvenkat@gmail.com>
2025-03-02 13:14:40 +0530

    updates

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+const productsRouter = require('./Routes/Products.route')
+
+app.use(cors());
+
+
+
+mongoose.connect('mongodb+srv://cluster0.y70bk.mongodb.net/seeit', {
+    dbName: 'seeit',
+    pass: 'bT7jfxiGZkWQ8yId',
+    user: 'chaitanyasai',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+app.use('/products', productsRouter);
+// app.use('/products/:id', productsRouter);
+
+
